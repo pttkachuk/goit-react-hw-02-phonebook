65f407189862e6992359c8aeeb53ddab3c9211bd
pttkachuk <petrotkachuk6@gmail.com>
2023-07-11 10:16:50 +0200

    add contactlist file

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,35 @@
+import PropTypes from 'prop-types';
+
+const ContactList = ({ contacts, handleDeleteContact }) => {
+    return (
+        <ul>
+            {contacts.map(({ id, number, name }) => {
+                return (
+                    <li key={id}>
+                        <p>{name + ':' + number}</p>
+                        <button
+                            type="button"
+                            name="delete"
+                            onClick={() => handleDeleteContact(id)}
+                        >
+                            Delete
+                        </button>
+                    </li>
+                )
+            })}
+        </ul>
+    )
+};
+
+export default ContactList;
+
+ContactList.propTypes = {
+    contacts: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.string.isRequired,
+            name: PropTypes.string.isRequired,
+            number: PropTypes.string.isRequired,
+        }).isRequired
+    ),
+    handleDeleteContact: PropTypes.func.isRequired,
+};
